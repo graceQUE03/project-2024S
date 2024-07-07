60866650cf46866d44ea4a0aff748dfdae12980b
Jeffrey <cyanyxe@students.cs.ubc.ca>
2024-07-07 13:18:03 -0700

    spaces are the devil

--- dCode/server.js
+++ dCode/server.js
@@ -12,16 +12,16 @@ const config = {
 	baseURL: 'http://localhost:3000',
 	clientID: 'XUZuvE6pKXJ1di0ZaTnBNpfzxFswNHxI',
 	issuerBaseURL: 'https://dev-6omitcvbhwq5hvfk.us.auth0.com'
-  };
-  
-  // auth router attaches /login, /logout, and /callback routes to the baseURL
-  app.use(auth(config));
-  
-  // req.isAuthenticated is provided from the auth router
-  app.get('/', (req, res) => {
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
 	console.log(req.oidc.isAuthenticated());
 	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
+});
 
 app.listen(3000, () => {
 	console.log('Server is listening on port 3000');
