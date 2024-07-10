21160adeb9b8a4ee431f1b8dca4a53ea562ae9b0
hxue03 <XYZxhy@170071>
2024-07-10 00:54:22 -0700

    add logout button to toolbar

--- dCode/src/home.tsx
+++ dCode/src/home.tsx
@@ -4,15 +4,32 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import { useNavigate } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
 
 function Home() {
+  const navigate = useNavigate();  
+
+  const handleLogout = () => {
+    navigate('/logout');
+    window.location.reload();
+    navigate('/');
+  };
+
   return (
     <div>
       <AppBar>
         <Toolbar>
+          <Grid container justifyContent="space-between" alignItems="center">
+          <Grid>
           <Button color="inherit" component={Link} to="/dcode/problems">Problems</Button>
           <Button color="inherit" component={Link} to="/dcode/placement">Placement</Button>
           <Button color="inherit" component={Link} to="/dcode/about">About</Button>
+          </Grid>
+          <Grid sx={{display: "flex", justifyContent: "center", height: 35, width: 80, borderRadius: "8px", bgcolor: "#ee4444"}}>
+          <Button color="inherit" onClick={handleLogout}>Logout</Button>
+          </Grid>
+          </Grid> 
         </Toolbar>
       </AppBar>
       <Container>
--- dCode/src/problems.tsx
+++ dCode/src/problems.tsx
@@ -1,19 +1,9 @@
 import './App.css';
-import { useNavigate } from 'react-router-dom';  
 
 function Problems() {
-  const navigate = useNavigate();  
-
-  const handleLogout = () => {
-    navigate('/logout');
-    window.location.reload();
-    navigate('/');
-  };
-
   return (
     <div className="App">
       <h1>Problems Page</h1>
-      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
