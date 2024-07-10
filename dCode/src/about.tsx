e2b17aa2d62051de5d1445861124da3c72f3aef4
hxue03 <XYZxhy@170071>
2024-07-10 16:00:20 -0700

    add About page

--- dCode/src/about.tsx
+++ dCode/src/about.tsx
@@ -1,9 +1,18 @@
 import './App.css';
+import { Box} from "@mui/material";
 
 function About() {
   return (
-    <div className="App">
-      <h1>About Page</h1>
+    <div className="App" style={{ width: '130vh', height: '100vh', paddingTop : '10vh', display: 'flex', flexDirection: 'column' }}>
+      <Box>
+      <h2>About Us</h2>
+      <h3>dcode is an education application that allows users to practice and enhance their code comprehension skills.</h3>
+      </Box>
+      <Box sx ={{paddingTop : '10vh', }}>
+      <h2>Feature</h2>
+      <h3>Our application will support questions of varying difficulty, and each question will be tagged with an appropriate label. </h3>
+      </Box>
+      
     </div>
   );
 }
