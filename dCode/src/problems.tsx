1878f7308032b33b6bd47ebe85f8eb5f5f10a25b
Dhara <illuviawriter@gmail.com>
2024-07-09 17:58:36 -0700

    update landing page

--- dCode/src/problems.tsx
+++ dCode/src/problems.tsx
@@ -1,10 +1,19 @@
-// problems.tsx
 import './App.css';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
 
 function Problems() {
+  const navigate = useNavigate();  
+
+  const handleLogout = () => {
+    navigate('/logout');
+    window.location.reload();
+    navigate('/');
+  };
+
   return (
     <div className="App">
       <h1>Problems Page</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
